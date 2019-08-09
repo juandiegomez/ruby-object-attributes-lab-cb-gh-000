d45392e517380238c60f5dc7f9c99f4786646f05
Juan Diego Gomez Peña <juandi.gomezp@utadeo.edu.co>
2019-08-09 18:05:05 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,17 +1,17 @@
 class Person
-  def name
-    @name
-  end
+  def name=(person_name)
+  @name = person_name
+end
 
-  def name = (name)
-    @name = name
-  end
+ def name
+  @name
+end
 
-  def job
-    @job
-  end
+ def job=(person_job)
+  @job = person_job
+end
 
-  def job = (job)
-    @job = job
-  end
+ def job
+   @job
+ end
 end
