cb4803308de850f1db1190aedca89544bf4e6f5e
Juan Diego Gomez Peña <juandi.gomezp@utadeo.edu.co>
2019-08-09 18:03:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,17 @@
 class Dog
+  def name = (dog_name)
+    @name = dog_name
+  end
+
   def name
     @name
   end
 
-  def name = (name)
-    @name = name
+  def breed = (dog_breed)
+    @breed = dog_breed
   end
 
-  def breed = (breed)
-    @breed = breed
+  def breed
+    @breed
   end
 end
